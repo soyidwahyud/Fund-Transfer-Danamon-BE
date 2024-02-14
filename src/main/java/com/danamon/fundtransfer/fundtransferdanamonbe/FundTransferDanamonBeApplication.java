7b264b90ee735e749f2973998bf3fea97140b300
soyidwahyud <soyidwahyu4@gmail.com>
2024-02-14 12:19:12 +0700

    Initial commit

---
+++ src/main/java/com/danamon/fundtransfer/fundtransferdanamonbe/FundTransferDanamonBeApplication.java
@@ -0,0 +1,13 @@
+package com.danamon.fundtransfer.fundtransferdanamonbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundTransferDanamonBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundTransferDanamonBeApplication.class, args);
+    }
+
+}
